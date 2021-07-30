5059a7740628dde3bf5fdf2b1cf60f0be57da3e6
TiberXiang <62795890+TiberXiang@users.noreply.github.com>
2021-07-30 23:25:15 +0800

    Add files via upload

---
+++ M1T1 source code.cpp
@@ -0,0 +1,19 @@
+int Light;
+void setup(){
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+  
+}
+
+void loop(){
+  Light = analogRead(A0);
+  if(Light > 150){
+    digitalWrite(13, HIGH);
+    Serial.println("High Lightness");
+  }else{
+    digitalWrite(13, LOW);
+    Serial.println("Low Lightness");
+  }
+  Serial.println(Light);
+  delay(1000);
+}
